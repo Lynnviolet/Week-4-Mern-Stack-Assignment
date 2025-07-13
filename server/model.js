05d18d6f347ea34247d3ad25f2d6a8a35917df84
LYNN VIOLET WANJIRU <shikkslynn@gmail.com>
2025-07-13 21:12:37 +0300

    Create model.js

---
+++ server/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+}, { timestamps: true });
+
+const CategorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+});
+
+const Post = mongoose.model('Post', PostSchema);
+const Category = mongoose.model('Category', CategorySchema);
+
+module.exports = { Post, Category };
